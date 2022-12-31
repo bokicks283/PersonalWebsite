5e5aebce25e6537852ba2b8a2b3521ddfacdbda0
bokicks283 <rbocchichio@gmail.com>
2022-12-31 00:10:01 -0500

    Added to nav bar and defined some css for it.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,8 @@
 import React from "react"
-
+import reactLogo from "./logo.svg"
 class NavBar extends React.Component {
     render() {
-        return <div class="nav-bar">This is my nav bar bitttttttttch</div>
+        return <div class="nav-bar"><div class="nav-bar-text"><p class="nav-bar-text">This is my nav bar bitttttttttch</p></div><img class="nav-search-icon" src={reactLogo} alt=""/></div>
     }
 } 
 
